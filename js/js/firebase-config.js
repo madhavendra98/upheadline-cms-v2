c06ff37a52a05c9a2f35b4a16658e506fd8c602f
madhavendra98 <smartmadhavendra@gmail.com>
2026-07-08 15:39:56 +0530

    Update firebase-config.js

--- js/js/firebase-config.js
+++ js/js/firebase-config.js
@@ -1,18 +1,1 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC0DUG3ABj31yaMLZxE2gYTseEYOgQ4_4U",
-  authDomain: "upheadlinenews-89d83.firebaseapp.com",
-  databaseURL: "https://upheadlinenews-89d83-default-rtdb.firebaseio.com",
-  projectId: "upheadlinenews-89d83",
-  storageBucket: "upheadlinenews-89d83.firebasestorage.app",
-  messagingSenderId: "366107139152",
-  appId: "1:366107139152:web:e8d67c7da7c85116610555"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getDatabase(app);
-
-export { db };
